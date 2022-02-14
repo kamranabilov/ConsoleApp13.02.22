4467b3cf96ff63f1b1ed927d704a864f56cfe2ea
kamranabilov <kamrangab@code.edu.az>
2022-02-14 09:03:26 +0400

    TASKadded

---
+++ 10fevral/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace ConsoleApp13._02._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Divid();
+        }
+
+
+
+        public static void Divid()
+
+        {
+            Console.WriteLine("number enter");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int b = a;
+            int c = a;
+
+            int number = a % 7;
+            int remain;
+
+            while (number != 0)
+            {
+                c--;
+                remain = c % 7;
+                if (remain == 0)
+                {
+                    break;
+                }
+            }
+            while (number != 0)
+            {
+                b++;
+                remain = b % 7;
+                if (remain == 0)
+                {
+                    break;
+                }
+
+            }
+
+            Console.WriteLine("7 ye bolunur");
+
+            int differ = a - c;
+            int differ1 = b - a;
+
+            if (differ > differ1)
+            {
+                Console.WriteLine($"result: {b}");
+            }
+            else
+            {
+                Console.WriteLine($"result: {c}");
+            }
+
+
+
+            operation();
+        }
+
+
+        public static void operation()
+        {
+            Console.WriteLine("enter number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int n = 15;
+            int m = 25;
+            int i = n + m;
+            Console.WriteLine(i);
+
+
+
+
+
+        }
+
+
+        }
+  
+}
+
